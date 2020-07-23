55f30278239b53c355bb545cd0604f1b01423e28
vntalking <anhson.duong@outlook.com>
2020-07-23 14:12:43 +0700

    Change text

--- chap3/todo-app/src/components/TodoApp.js
+++ chap3/todo-app/src/components/TodoApp.js
@@ -22,7 +22,7 @@ class TodoApp extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello from Create React App</h1>
+                <h1>Hello from VNTALKING</h1>
                 <p>I am in a React Component!</p>
             </div>
         );
