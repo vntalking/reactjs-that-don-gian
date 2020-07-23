035f0618d1842b3207d86da8fe22892c5bbeb115
vntalking <anhson.duong@outlook.com>
2020-07-24 00:10:14 +0700

    Add style todoItem

--- chap3/todo-app/src/components/TodoApp.js
+++ chap3/todo-app/src/components/TodoApp.js
@@ -1,32 +1,32 @@
 import React from "react";
-// import Todos from "./Todos";
+import Todos from "./Todos";
 import Header from "../components/layout/Header";
 
 class TodoApp extends React.Component {
-    // state = {
-    //     todos: [
-    //         {
-    //             id: 1,
-    //             title: "Setup development environment",
-    //             completed: true
-    //         },
-    //         {
-    //             id: 2,
-    //             title: "Develop website and add content",
-    //             completed: false
-    //         },
-    //         {
-    //             id: 3,
-    //             title: "Deploy to live server",
-    //             completed: false
-    //         }
-    //     ]
-    // };
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: "Setup development environment",
+                completed: true
+            },
+            {
+                id: 2,
+                title: "Develop website and add content",
+                completed: false
+            },
+            {
+                id: 3,
+                title: "Deploy to live server",
+                completed: false
+            }
+        ]
+    };
     render() {
         return (
-            <div>
-                <Header/>
-                {/* <Todos todos={this.state.todos} /> */}
+            <div className="container">
+                <Header />
+                <Todos todos={this.state.todos} />
             </div>
         );
     }
--- chap3/todo-app/src/components/Todos.js
+++ chap3/todo-app/src/components/Todos.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 
 class Todos extends React.Component {
     render() {
@@ -6,7 +7,7 @@ class Todos extends React.Component {
             <div>
                 <ul>
                     {this.props.todos.map(todo => (
-                        <li key={todo.id}>{todo.title}</li>
+                        <TodoItem key={todo.id} todo={todo} />
                     ))}
                 </ul>
             </div>
