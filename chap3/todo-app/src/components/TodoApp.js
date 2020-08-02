4c0ab9cae11cd5c3aa95592cec302a1a26774473
vntalking <anhson.duong@outlook.com>
2020-08-02 14:05:41 +0700

    Convert TodoApp to Hooks

--- chap3/todo-app/src/components/TodoApp.js
+++ chap3/todo-app/src/components/TodoApp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Todos from "./Todos";
 import Header from "../components/layout/Header";
 import AddTodo from "./AddTodo"
@@ -6,13 +6,14 @@ import AddTodo from "./AddTodo"
 //khai bao thu vien axios
 import axios from "axios";
 
-class TodoApp extends React.Component {
-    state = {
+function TodoApp() {
+
+    const [state, setState] = useState({
         todos: []
-    };
-    handleCheckboxChange = id => {
-        this.setState({
-            todos: this.state.todos.map(todo => {
+    });
+    const handleCheckboxChange = id => {
+        setState({
+            todos: state.todos.map(todo => {
                 if (todo.id === id) {
                     todo.completed = !todo.completed;
                 }
@@ -20,18 +21,18 @@ class TodoApp extends React.Component {
             })
         });
     };
-    deleteTodo = id => {
+    const deleteTodo = id => {
         axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-            .then(reponse => this.setState({
+            .then(reponse => setState({
                 todos: [
-                    ...this.state.todos.filter(todo => {
+                    ...state.todos.filter(todo => {
                         return todo.id !== id;
                     })
                 ]
             }))
     };
 
-    addTodo = title => {
+    const addTodo = title => {
         const todoData = {
             title: title,
             completed: false
@@ -39,14 +40,14 @@ class TodoApp extends React.Component {
         axios.post("https://jsonplaceholder.typicode.com/todos", todoData)
             .then(response => {
                 console.log(response.data)
-                this.setState({
-                    todos: [...this.state.todos, response.data]
+                setState({
+                    todos: [...state.todos, response.data]
                 })
             });
     };
 
 
-    componentDidMount() {
+    useEffect(() => {
         const config = {
             params: {
                 _limit: 5
@@ -55,19 +56,19 @@ class TodoApp extends React.Component {
         }
         //tao GET request de lay danh sach todos
         axios.get("https://jsonplaceholder.typicode.com/todos", config)
-            .then(response => this.setState({ todos: response.data }));
-    }
+            .then(response => setState({ todos: response.data }));
+    }, []);
 
-    render() {
-        return (
+    
+    return (
             <div className="container">
                 <Header />
-                <AddTodo addTodo={this.addTodo} />
-                <Todos todos={this.state.todos}
-                    handleChange={this.handleCheckboxChange}
-                    deleteTodo={this.deleteTodo} />
+                <AddTodo addTodo={addTodo} />
+                <Todos todos={state.todos}
+                    handleChange={handleCheckboxChange}
+                    deleteTodo={deleteTodo} />
             </div>
-        );
-    }
+    );
+    
 }
 export default TodoApp;
